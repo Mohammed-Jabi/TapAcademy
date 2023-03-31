259b2ad9cc1924743a681c49d71fda92f5356b7d
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-03-31 22:54:52 +0530

    Polymorifism

---
+++ src/com/Tap/OOPs/Polimorfism.java
@@ -0,0 +1,28 @@
+package com.Tap.OOPs;
+
+class Plane{
+	
+}
+
+class CargoPlane extends Plane{
+	
+}
+
+class PasengerPlane extends Plane{
+	
+}
+
+class FighterPlane extends Plane{
+	
+}
+
+
+
+public class Polimorfism {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
