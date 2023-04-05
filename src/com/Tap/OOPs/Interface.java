43f40ef80252eeb4f3613c61e975fd89f4a0e179
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-06 00:01:24 +0530

    Interface class

---
+++ src/com/Tap/OOPs/Interface.java
@@ -0,0 +1,58 @@
+package com.Tap.OOPs;
+
+interface Calculator {
+	
+	void add();
+	void sub();
+
+}
+
+
+class Calculator1 implements Calculator{
+
+	public void add() {
+		
+	}
+
+	public void sub() {
+		
+	}
+	
+}
+
+class Calculator2 implements Calculator{
+
+	public void add() {
+
+		
+	}
+
+	public void sub() {
+
+		
+	}
+	
+}
+
+class Calculator3 implements Calculator{
+
+	public void add() {
+
+		
+	}
+
+
+	public void sub() {
+
+		
+	}
+	
+}
+
+public class Interface {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
