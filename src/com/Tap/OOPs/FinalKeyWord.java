3791f6e99614b3a81737056725a0bf819cef5d4f
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-06 23:58:16 +0530

    Final Key Word

---
+++ src/com/Tap/OOPs/FinalKeyWord.java
@@ -0,0 +1,29 @@
+package com.Tap.OOPs;
+
+class Restorent{
+	
+}
+
+class Res1{
+	
+}
+
+class Res2{
+	
+}
+
+class Res3{
+	
+}
+
+class Food{
+	
+}
+
+public class FinalKeyWord {
+
+	public static void main (String[] args) {
+		
+	}
+	
+}
