69f99257c0a4febd7425eea6a974ffb746d558c6
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-15 12:45:46 +0530

    Custome Eception

---
+++ src/com/Tap/ExceptionHandling/CustomException.java
@@ -0,0 +1,22 @@
+package com.Tap.ExceptionHandling;
+
+class Atm{
+	
+	
+	
+}
+
+class Bank{
+	
+	
+	
+}
+
+public class CustomException {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
