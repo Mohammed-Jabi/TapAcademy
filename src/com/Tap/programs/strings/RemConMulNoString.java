2c63d608258f09f3aef99d47b2f35ae2f67eb17c
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-22 01:26:09 +0530

    String programs

---
+++ src/com/Tap/programs/strings/RemConMulNoString.java
@@ -0,0 +1,37 @@
+package com.Tap.programs.strings;
+
+import java.util.Scanner;
+
+public class RemConMulNoString {
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		String t = scan.nextLine();
+		string2in1(s,t);
+
+
+	}
+
+	static void string2in1(String s, String t) {
+
+		int i=0, j=0;
+		
+		while(j<t.length() && i<s.length()) {
+			if(s.charAt(i) == t.charAt(j)) {
+				i++;
+				j++;
+			}
+			else {
+				i++;
+			}
+		}
+		if(j == t.length()) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+}
