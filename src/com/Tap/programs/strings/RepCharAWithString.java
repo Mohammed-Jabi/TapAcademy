763a65e4cb7a9486df4e148e6fba87fa73fbb78f
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-21 09:17:51 +0530

    STring programs

---
+++ src/com/Tap/programs/strings/RepCharAWithString.java
@@ -0,0 +1,30 @@
+package com.Tap.programs.strings;
+
+import java.util.Scanner;
+
+public class RepCharAWithString {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+
+		removeLowerChar(s);
+
+
+	}
+
+	private static void removeLowerChar(String s) {
+		String t="";
+		for(int i=0; i<s.length(); i++) {
+			char ch=s.charAt(i);
+			if(ch=='a' || ch=='A' ) {
+				t=t+'*'+s.charAt(i);
+			}
+			else {
+				t=t+ch;
+			}
+		}
+		System.out.println(t);
+	}
+}
