5f99fb38378a2568335ae2f303a0b82751933af9
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-24 22:53:54 +0530

    String programing class

---
+++ src/com/Tap/programs/strings/SubStrings.java
@@ -0,0 +1,24 @@
+package com.Tap.programs.strings;
+
+import java.util.Scanner;
+
+public class SubStrings {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		int n=scan.nextInt();
+		subString(s, n);
+		
+	}
+
+	static void subString(String s, int n) {
+	
+		for(int i=0; i<=s.length()-n; i++) {
+			
+		}
+		
+	}
+	
+}
