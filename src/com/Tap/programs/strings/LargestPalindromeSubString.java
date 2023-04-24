88dd4848d5fae732cc3203d2aeff3569c93afa05
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-24 23:41:03 +0530

    String programming class

---
+++ src/com/Tap/programs/strings/LargestPalindromeSubString.java
@@ -0,0 +1,48 @@
+package com.Tap.programs.strings;
+
+import java.util.Scanner;
+
+public class LargestPalindromeSubString {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		allAubStringPalindrom(s);
+
+
+	}
+
+	static void allAubStringPalindrom(String s) {
+
+		for(int i=s.length(); i>0; i++) {
+			for(int j=0; j<=s.length()-i; j++) {
+				String t = "";
+				for(int k=j; k<i+j; k++) {
+					t=t+s.charAt(k);
+				}
+				if(palindrome(t)) {
+					System.out.println(t);
+					return;	
+				}
+			}
+		}
+
+	}
+
+
+	static boolean palindrome(String t) {
+
+		int i=0, j=t.length()-1;
+		while(i<j) {
+			if(t.charAt(i) != t.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+
+}
+
