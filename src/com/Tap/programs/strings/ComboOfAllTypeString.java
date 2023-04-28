eef792dbdb782ff60e4d48521fdd44ef44ec7f85
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-04-28 22:00:43 +0530

    Programming class Strng

---
+++ src/com/Tap/programs/strings/ComboOfAllTypeString.java
@@ -0,0 +1,17 @@
+package com.Tap.programs.strings;
+
+import java.util.Scanner;
+
+public class ComboOfAllTypeString {
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+
+		removeLowerChar(s);
+
+
+	}
+
+}
