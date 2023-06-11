71f44fd41cbb53c72f49194c6954023122978fb5
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-06-11 11:25:12 +0530

    Singletone design pattern and synchronous key word in treds

--- src/com/Tap/Designpattern/SingleToneDS.java
+++ src/com/Tap/Designpattern/SingleToneDS.java
@@ -1,10 +1,31 @@
 package com.Tap.Designpattern;
 
+class Alpha{
+	
+	static Alpha alpha;
+	
+	private Alpha() {
+		
+	}
+	//for multitreded program
+	synchronized public static Alpha getAlpha(){
+		if(alpha==null) {
+			alpha = new Alpha();
+		}
+		return alpha;
+	}
+	
+}
+
 public class SingleToneDS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	Alpha a1 =	Alpha.getAlpha();
+	Alpha a2 =	Alpha.getAlpha();
+	
+	System.out.println(a1);
+	System.out.println(a2);
 	}
 
 }
--- src/com/Tap/multitreding/MultiterdingBank.java
+++ src/com/Tap/multitreding/MultiterdingBank.java
@@ -1,9 +1,15 @@
  package com.Tap.multitreding;
  
  class Customer{
-	 public void withdraw(String mode) {
+	 synchronized void withdraw(String mode) {
 		for(int i=0; i<=10; i++) {
 			System.out.println("Withdraw through"+mode);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
  }
@@ -35,6 +41,10 @@ public class MultiterdingBank {
 	Bank t1 =	new Bank(alex,"PhonePe");
 	Bank t2 =	new Bank(alex,"Paytm");
 	Bank t3 =	new Bank(alex,"G-Pay");
+	
+	t1.start();
+	t2.start();
+	t3.start();
 
 	}
 
