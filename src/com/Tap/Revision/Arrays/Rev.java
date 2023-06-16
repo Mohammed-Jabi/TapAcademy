9fd60dab5973f3519844ff762a576bac0f53f7c5
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-06-16 20:10:55 +0530

    Updated rev

---
+++ src/com/Tap/Revision/Arrays/Rev.java
@@ -0,0 +1,32 @@
+package com.Tap.Revision.Arrays;
+
+public class Rev {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Hello";
+		String str2 = "Hello";
+		System.out.println(str1 == str2);
+		
+		 final int MAX_VALUE = 100;
+		
+		 int x = 5;
+		 System.out.println(x++);
+		 System.out.println(x);
+		 
+//	  myVariable;
+//		  123variable;
+//		 _myVariable;
+//		  $myVariable;
+		  
+		  int x1 = 5;
+		  int y = 2;
+		  System.out.println(x1 / y);
+		  
+		  String str = "Hello";
+		  System.out.println(str.length());
+
+
+	}
+
+}
